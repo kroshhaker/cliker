048b92091d719a42c053e9870883056ebb0e3706
kroshhaker <amir890466@gmail.com>
2023-06-07 19:30:58 +0300

    v1.1.0

--- app/src/main/java/ru/Krosh/cliker/MainActivity.kt
+++ app/src/main/java/ru/Krosh/cliker/MainActivity.kt
@@ -1,23 +1,39 @@
 package ru.Krosh.cliker
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val torch: Button = findViewById(R.id.buttonTorch)
-        val scoreView: TextView = findViewById(R.id.Score)
-        val icon: ImageView = findViewById(R.id.moneta)
-        var score: Int = 0
-        torch.setOnClickListener {
-            score = score + 1
+        val balView: TextView = findViewById(R.id.Score)
+        val shop: Button = findViewById(R.id.shop)
+        var bal: Int = 0
+        var cost: Int = 500
+        var u: Int = 0
+        torch.setOnClickListener{
+            bal += (1 + u)
 
-            scoreView.text = "Баланс: " + score.toString() + "$"
+            balView.text = "Баланс: $bal$"
+        }
+        shop.setOnClickListener{
+            if(bal >= cost){
+                bal -= cost
+                u++
+                cost *= 2
+                shop.text = "Купить улучшение (+${u+1})\nСтоимость: $cost$"
+                Toast.makeText(applicationContext, "Вы купили улучшение.", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext, "Недостаточно средст.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
